4d2733c4f819de14ae2a6202fd69334f292e74d4
BrayanOmarTobiasOrnelas_222773 <al222773@alumnos.uacj.mx>
2025-04-23 17:19:16 -0600

    Creacion de ventana inventario

--- Header.py
+++ Header.py
@@ -108,8 +108,19 @@ class VentasPage(ctk.CTkFrame):
 class InventarioPage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master, fg_color="white")
-        label = ctk.CTkLabel(self, text="Página de Inventario", font=("Arial", 24))
-        label.pack(pady=50)
+
+        # Encabezados
+        headers = ["Productos", "Entradas", "Salidas", "Sobrantes", "Cantidades", "Stock"]
+        ancho_columnas = [150, 100, 100, 100, 100, 100]
+
+        self.tabla_frame = ctk.CTkFrame(self, fg_color="#f4f4f4", corner_radius=10)
+        self.tabla_frame.pack(padx=20, pady=10, fill="both", expand=False)
+
+        for i, header in enumerate(headers):
+            label = ctk.CTkLabel(self.tabla_frame, text=header, font=("Arial", 16, "bold"), text_color="black")
+            label.grid(row=0, column=i, padx=5, pady=10)
+        
+        
 
 # Pagina de Reportes
 class ReportesPage(ctk.CTkFrame):
